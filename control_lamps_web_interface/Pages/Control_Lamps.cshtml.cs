d0e3eeea922c50c7ee8ce5a5874584879c01a07a
delli <matteo.delli.rocioli.pe@gmail.com>
2019-06-05 10:16:22 +0200

    first implementation of the web form

---
+++ control_lamps_web_interface/Pages/Control_Lamps.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using message_service.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace control_lamps_web_interface.Pages
+{
+    public class Control_LampsModel : PageModel
+    {
+        [BindProperty]
+        public Lamp Input { get; set; }
+
+        public void OnGet()
+        {
+            Input = new Lamp();
+        }
+
+        public async Task OnPost()
+        {
+            var lamp = new Lamp
+            {
+                Name = Input.Name,
+                Color= Input.Color
+            };
+
+            RedirectToPage();
+        }
+    }
+}
---
+++ message_service/Models/Lamp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace message_service.Models
+{
+    public class Lamp
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Color { get; set; }
+    }
+}
