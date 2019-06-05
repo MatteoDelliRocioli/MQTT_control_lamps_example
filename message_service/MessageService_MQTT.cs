0ffabbaced4df7972dc8b6552cece42dc0b3a85f
lorenzo97 <rossetto.lorenzo97@gmail.com>
2019-06-05 13:07:24 +0200

    changes on MessageService_MQTT

--- message_service/MessageService_MQTT.cs
+++ message_service/MessageService_MQTT.cs
@@ -1,5 +1,6 @@
 ﻿using message_service.Models;
 using System;
+using System.Text;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
 
@@ -7,22 +8,7 @@ namespace message_service
 {
     class MessageService_MQTT : IMessageService <Lamp>
     {
-        static void Main(string[] args)
-        {
-            // Create Client instance
-            MqttClient myClient = new MqttClient("test.mosquitto.org");
-
-            // Register to message received
-            myClient.MqttMsgPublishReceived += client_recievedMessage;
-
-            string clientId = Guid.NewGuid().ToString();
-            myClient.Connect(clientId);
-
-            // Subscribe to topic
-            myClient.Subscribe(new String[] { "pncGroup/lampId/color" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
-            System.Console.ReadLine();
-        }
-
+        public MqttClient Client;
 
         static void client_recievedMessage(object sender, MqttMsgPublishEventArgs e)
         {
@@ -33,13 +19,34 @@ namespace message_service
 
         public void Connect()
         {
-            
-        }
+            // Create Client instance
+            Client = new MqttClient("test.mosquitto.org");
 
+            // Register to message received
+            Client.MqttMsgPublishReceived += client_recievedMessage;
+
+            string clientId = Guid.NewGuid().ToString();
+            Client.Connect(clientId);
+
+           
+        }
+        // Subscribe to topic
+        /*
+        myClient.Subscribe(new String[] { "pncGroup/lampId/color" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE
+});
+            System.Console.ReadLine();
+            */
         public void Send(Lamp message)
         {
-            
+            Client = new MqttClient("test.mosquitto.org");
+
+            Client.MqttMsgPublished += client_MqttMsgPublished;
+
+            ushort msgId = Client.Publish("pncGroup/lampId/color", // topic
+               Encoding.UTF8.GetBytes("MyMessageBody"), // message body
+               MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, // QoS level
+               false); // retained
         }
     }
 }
-}
+
