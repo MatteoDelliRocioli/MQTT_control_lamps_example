9f4a77badc5358c51b96284677bee79be169553e
delli <matteo.delli.rocioli.pe@gmail.com>
2019-06-05 09:35:18 +0200

    Add project files.

---
+++ MQTT_control_lamps_example/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MQTT_control_lamps_example.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
